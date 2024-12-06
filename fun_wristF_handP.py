c10b3ddbddfb7371b11236c51f7202deb1c8f282
Hugo <167751232+HugoFrancalanci@users.noreply.github.com>
2024-12-06 13:49:48 +0100

    Update fun_wristF_handP.py

--- fun_wristF_handP.py
+++ fun_wristF_handP.py
@@ -1,12 +1,10 @@
-#VALIDE
-
 import kineticstoolkit.lab as ktk
 import matplotlib.pyplot as plt
 import numpy as np
 from fun_XprocessData import obtenir_max_points
 
 c3d_filenames = [
-    r"C:\Users\Francalanci Hugo\Desktop\CMP II (10)\Mémoire\Protocole\Motion capture\Code traitement données\Python\Code 3D\Final\habiletes_tests\THM_S1_M8.c3d"
+    #chemin
 ] 
  
 num_points = obtenir_max_points(c3d_filenames)
